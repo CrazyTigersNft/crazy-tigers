36a3160b9764dbf120a6898981f5da494b548537
Tickether <tickether@gmail.com>
2022-08-12 13:38:14 +0000

    margin about page

--- src/About.js
+++ src/About.js
@@ -7,9 +7,10 @@ function About() {
     <div className='about-moving-backround'>
       <Image className='headings' src={AboutTiger}/>
         <Flex justify='center' align='center' height='40vh' paddingBottom='150px'>
-            <Box width='520px'>
+            <Box marginTop='250px' width='520px'>
                 <div>
-                    <Text>3333 Crazy Tigers live on Planet Panthera, it is under the threat of alien invasion. The Aliens abduct tigers putting them to work on their giant space stations. Tigers live their lives as normal most of the time, working, having fun and making cubs ;). Sometimes they are recruited to go into space to fight off alien attacks. Their currency is $meat coin that they work to buy food - meat.</Text>
+                    <Text>3333 Crazy Tigers live on Planet Panthera, it is under the threat of alien invasion. The Aliens abduct tigers putting them to work on their giant space stations. </Text>
+                    <Text>Tigers live their lives as normal most of the time, working, having fun and making cubs ;). Sometimes they are recruited to go into space to fight off alien attacks. Their currency is $meat coin that they work to buy food - meat.</Text>
                 </div>
             </Box>
         </Flex>
