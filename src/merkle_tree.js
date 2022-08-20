05020f744089c06821395955c0236e44c1b7b0c1
Tickether <tickether@gmail.com>
2022-08-20 14:32:32 +0000

    final merkle list

--- src/merkle_tree.js
+++ src/merkle_tree.js
@@ -18,7 +18,7 @@ const rootHash = merkleTree.getRoot();
 console.log('WL merkle tree/n', merkleTree.toString());
 console.log('root hash: ', rootHash )
 
-const clamingAddress = leafNodes[0]; 
+const clamingAddress = leafNodes[6770]; 
 
 
 
@@ -30,19 +30,12 @@ let hexProof = "[" + hex_Proof + "]";
 console.log('hex Proof: ',hexProof)
 console.log('hex Proof: ',hexproof)
 //let index = allowListAddresses.indexOf(address);
-let index = allowListAddresses.indexOf("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2")
+let index = allowListAddresses.indexOf("0x4f0AD22Da38Ba8dcE5B953Dc24917DdC64C67459")
 
 console.log(index)
 //console.log(leafNodes[2])
 
 console.log(merkleTree.verify(hexproof, clamingAddress, rootHash));
+console.log('root hash: ', rootHash )
 
 
-
-/*
-[
-  "0x04a10bfd00977f54cc3450c9b25c9b3a502a089eba0097ba35fc33c4ea5fcb54",
-  "0x455e3e1e9d547a8f089eb3c1aa33c26e60ab0f0e0fa382f2b4c41e741c034ba1",
-  "0xda2a605bdf59a3b18e24cd0b2d9110b6ffa2340f6f67bc48214ac70e49d12770"
-]
-*/
